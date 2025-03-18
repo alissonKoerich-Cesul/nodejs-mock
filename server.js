c683c0f180704c5caa0f3961135545a16073dedc
alissonkoerich <alisson.2023014305@alunocesul.com>
2025-03-18 19:35:11 -0300

    feat: atualizado codigos status

--- server.js
+++ server.js
@@ -3,17 +3,7 @@ const express= require('express');
 const app = express();
 const port = 3000;
 
-const products = [ "Camiseta",
-    "Calça Jeans",
-    "Tênis",
-    "Jaqueta de Couro",
-    "Óculos de Sol",
-    "Relógio",
-    "Bermuda",
-    "Boné",
-    "Mochila",
-    "Cinto"
-  ];
+const products = [ ];
 
 
 app.get('/',(req,res)=>{
@@ -21,20 +11,23 @@ app.get('/',(req,res)=>{
 });
 
 app.get('/products',(req,res)=>{
-
-    res.json(products).send();
+    const {id} = req.query;
+    res.json({id}).send();
 });
 
 
 app.post('/products',(req, res)=>{
     res.status(201).json(products).send();
 });
+app.put('/products',(req, res)=>{
+    res.status(204).json(products).send();
+});
 
 app.patch('/products',(req, res)=>{
-    res.status(201).json(products).send();
+    res.status(204).json(products).send();
 });
 app.delete('/products',(req, res)=>{
-    res.status(201).json(products).send();
+    res.status(204).json(products).send();
 });
 
 
